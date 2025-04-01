ef5ab96bc8a6c4bcd89904c68e46f1182c4341ad
MIDO <155222995+Midohajhouj@users.noreply.github.com>
2025-04-01 12:56:50 -0400

    Kali Linux Tools installer

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ INSTALL_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 # Create temp directory
 mkdir -p "$TEMP_DIR"
 
-# --------------------------
+# -------------------------
 #  Color Definitions
-# --------------------------
+# -------------------------
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
